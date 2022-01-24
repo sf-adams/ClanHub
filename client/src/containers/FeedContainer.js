7997551bbf6f8895cfa8820e266fd5b150c74fc6
Sam <samuel.fitz@btinternet.com>
2022-01-24 21:38:31 +0000

    Colours Tweaked

--- client/src/containers/FeedContainer.js
+++ client/src/containers/FeedContainer.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function FeedContainer() {
   return (
     <div>
-      <h1>This is the feed.</h1>
+      <h1>Welcome to ClanHub.</h1>
       <Link to="profile">Click to view your profile.</Link>
     </div>
   );
