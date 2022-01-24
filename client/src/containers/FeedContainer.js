a5ea7cf89920668fc14531adf61f2e2820a213ea
Sam <samuel.fitz@btinternet.com>
2022-01-24 20:58:39 +0000

    Links Test Setup

--- client/src/containers/FeedContainer.js
+++ client/src/containers/FeedContainer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function FeedContainer() {
   return (
     <div>
       <h1>This is the feed.</h1>
+      <Link to="/">Click to view your feed.</Link>
     </div>
   );
 }
--- client/src/containers/ProfileContainer.js
+++ client/src/containers/ProfileContainer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function ProfileContainer() {
   return (
     <div>
       <h1>This is the profile page.</h1>
+      <Link to="/">Click to view your feed.</Link>
     </div>
   );
 }
