a0b917c2c63f1fda71840cf83658c0ac3f437d68
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-01-24 22:58:20 +0000

    added registration function

--- client/src/components/login/Signup.js
+++ client/src/components/login/Signup.js
@@ -1,10 +1,20 @@
 import React, {useState, useEffect} from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 export default function Signup() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [passwordConfirm, setPasswordConfirm] = useState("");
+
+    const register = async ()=> {
+      try {
+        const user = await createUserWithEmailAndPassword()
+      }
+      catch(error) {
+        console.log(error.message)
+      }
+    }
 
     const handleEmailChange = (ev)=> {
       setEmail(ev.target.value)
