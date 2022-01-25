1de8c6bb06746266ef4b80df4bd96524211db9a2
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-01-25 10:22:36 +0000

    refactor done on login component

--- client/src/components/login/LoginComponent.js
+++ client/src/components/login/LoginComponent.js
@@ -7,7 +7,6 @@ export default function LoginComponent() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  // const [passwordConfirm, setPasswordConfirm] = useState("");
 
 
   const login = async () => {
@@ -35,9 +34,6 @@ export default function LoginComponent() {
     setPassword(ev.target.value);
   };
 
-  // const handlePasswordConfirmChange = (ev) => {
-  //   setPasswordConfirm(ev.target.value);
-  // };
 
   return (
     <>
@@ -62,7 +58,7 @@ export default function LoginComponent() {
           value={password}
           required
           autoComplete="off"
-          placeholder="Email"
+          placeholder="Password"
           onChange={handlePasswordChange}
         />
 
@@ -70,35 +66,6 @@ export default function LoginComponent() {
       </div>
 
       <button onClick={logout}> Sign Out </button>
-      {/* <div>
-        <h2>Login!</h2>
-        <form action="POST" className="group">
-          <div className="form-item email-group-login">
-            <label htmlFor="login-email">Email</label>
-            <input
-              type="email"
-              id="login-email"
-              name="email"
-              value={email}
-              required
-              autoComplete="off"
-              onChange={handleEmailChange}
-            />
-          </div>
-          <div className="group">
-            <label htmlFor="login-passwprd">Password</label>
-            <input
-              type="password"
-              id="login-password"
-              name="password"
-              value={password}
-              required
-              onChange={handlePasswordChange}
-            />
-          </div>
-          <button type="sumbit">Register</button>
-        </form>
-      </div> */}
     </>
   );
 }
--- client/src/containers/LoginContainer.js
+++ client/src/containers/LoginContainer.js
@@ -1,76 +1,16 @@
 import { useState } from "react";
 import Signup from "../components/login/Signup";
-import Login from '../components/login/LoginComponent'
-import {
-  createUserWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut,
-} from "firebase/auth";
-import { auth } from "../auth/firebase-config";
+import Login from "../components/login/LoginComponent";
 
 function App() {
-  // const [registerEmail, setRegisterEmail] = useState("");
-  // const [registerPassword, setRegisterPassword] = useState("");
-  // const [loginEmail, setLoginEmail] = useState("");
-  // const [loginPassword, setLoginPassword] = useState("");
 
   const [user, setUser] = useState({});
-
-  // onAuthStateChanged(auth, (currentUser) => {
-  //   setUser(currentUser);
-  // });
-
-  // const register = async () => {
-  //   try {
-  //     const user = await createUserWithEmailAndPassword(
-  //       auth,
-  //       registerEmail,
-  //       registerPassword
-  //     );
-  //     console.log(user);
-  //   } catch (error) {
-  //     console.log(error.message);
-  //   }
-  // };
-
-  
 
   return (
     <>
       <Signup />
-      <Login/>
-
-      
+      <Login />
     </>
   );
 }
-
 export default App;
-
-// import React, { useState } from "react";
-// import Signup from "../components/login/Signup";
-// import LoginComponent from "../components/login/LoginComponent";
-// import { onAuthStateChanged, signOut } from "firebase/auth";
-// import { auth } from "../auth/firebase-config";
-
-// export default function LoginContainer() {
-//   const [user, setUser] = useState({});
-
-//   const logout = async () => {
-//     await signOut(auth);
-//   };
-
-//   onAuthStateChanged(auth, (currentUser) => {
-//     setUser(currentUser);
-//   });
-
-//   return (
-//     <>
-//       <Signup />
-//       <LoginComponent />
-//       <h4>User signed in</h4>
-//       {user?.email}
-//       <button onClick = {logout}>Log out</button>
-//     </>
-//   );
-// }
