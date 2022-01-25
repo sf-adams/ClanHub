86187bf597dbe67077dd908d7ac975b4eca4bcdf
Sam <samuel.fitz@btinternet.com>
2022-01-25 14:23:29 +0000

    Adding Layout Pages

--- client/src/auth/useAuth.js
+++ client/src/auth/useAuth.js
@@ -12,3 +12,78 @@ firebase.initializeApp({
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
 });
+
+const AuthContext = createContext();
+
+// Hook for child components to get the auth object ...
+// ... and re-render when it changes.
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+// Provider hook that creates auth object and handles state
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+
+  // Wrap any Firebase methods we want to use making sure ...
+  // ... to save the user to state.
+  const sendSignInLinkToEmail = email => {
+    return firebase
+      .auth()
+      .sendSignInLinkToEmail(email, {
+        url: 'https://react-firebase-passwordless.vercel.app/confirm',
+        handleCodeInApp: true,
+      })
+      .then(() => {
+        return true;
+      });
+  };
+
+  const signInWithEmailLink = (email, code) => {
+    return firebase
+      .auth()
+      .signInWithEmailLink(email, code)
+      .then(result => {
+        setUser(result.user);
+        return true;
+      });
+  };
+
+  const logout = () => {
+    return firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        setUser(null);
+      });
+  };
+
+  // Subscribe to user on mount
+  // Because this sets state in the callback it will cause any ...
+  // ... component that utilizes this hook to re-render with the ...
+  // ... latest auth object.
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      setUser(user);
+      setIsAuthenticating(false);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
+  const values = {
+    user,
+    isAuthenticating,
+    sendSignInLinkToEmail,
+    signInWithEmailLink,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={values}>
+      {!isAuthenticating && children}
+    </AuthContext.Provider>
+  );
+};
