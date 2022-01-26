749e35129836d934950e5a4a314559df73060798
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-01-26 11:14:25 +0000

    addwd post class

--- server/src/main/java/com/clanhub/example/ClanHub/models/Post.java
+++ server/src/main/java/com/clanhub/example/ClanHub/models/Post.java
@@ -1,4 +1,78 @@
 package com.clanhub.example.ClanHub.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name= "category")
+    private String category;
+
+    @Column(name= "title")
+    private String title;
+
+    @Column(name= "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user")
+    @JsonIgnoreProperties({"post"})
+    private User user;
+
+    public Post(String category, String title, String description, User user) {
+        this.category = category;
+        this.title = title;
+        this.description = description;
+        this.user = user;
+    }
+
+    public Post() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- server/src/main/java/com/clanhub/example/ClanHub/repositories/UserRepository.java
+++ server/src/main/java/com/clanhub/example/ClanHub/repositories/UserRepository.java
@@ -1,4 +1,11 @@
 package com.clanhub.example.ClanHub.repositories;
 
-public class UserRepository {
+import com.clanhub.example.ClanHub.models.Post;
+import com.clanhub.example.ClanHub.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByPosts(List<Post> posts);
 }
