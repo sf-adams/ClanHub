4d77c80f6db95cd91b79976d88f41b07eaefc56d
Sam <samuel.fitz@btinternet.com>
2022-01-27 15:22:59 +0000

    Modifying Auth Context

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import "./styles/css/style.css";
 import { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  Redirect
+} from 'react-router-dom';
 import { Navigate } from "react-router-dom";
+import UserService from './services/UserService';
+
+// Container & Component Imports
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/navbar/Menu";
 import FeedContainer from "./containers/FeedContainer";
 import ProfileContainer from "./containers/ProfileContainer";
 import LoginContainer from "./containers/LoginContainer";
 import HomeContainer from "./containers/HomeContainer";
-import Navbar from "./components/navbar/Navbar";
-import Menu from "./components/navbar/Menu";
-import UserService from './services/UserService';
 
 // Authentication Imports
 import { auth } from "./auth/firebase-config";
@@ -16,6 +24,7 @@ import { AuthContextProvider } from "./auth/AuthContext";
 import { AuthenticatedRoute } from "./auth/AuthenticatedRoute";
 import { UnauthenticatedRoute } from "./auth/UnauthenticatedRoute";
 import { onAuthStateChanged } from "firebase/auth";
+import Signup from "./components/login/Signup";
 
 
 function App() {
@@ -25,64 +34,61 @@ function App() {
   const [users, setUsers] = useState([]);
   const [menuOpen, setMenuOpen] = useState(false);
 
-  useEffect(()=> {
-    UserService.getUsers().then((users)=> setUsers(users.data))
-    }, [])
+  // useEffect(()=> {
+  //   UserService.getUsers().then((users)=> setUsers(users.data))
+  //   }, [])
 
-  useEffect(()=> {
-    setLoggedIn(getLoggedIn)
-  }, [user])
+  // useEffect(()=> {
+  //   setLoggedIn(getLoggedIn)
+  // }, [user])
 
-  const getLoggedIn = ()=> {
-    if (users && user){
-      const sel = users.filter((user) => {
-        console.log(user.email);
-        return user.email === auth.currentUser.email;
-      });
-      return sel[0];
-    }
-  }
+  // const getLoggedIn = ()=> {
+  //   if (users && user){
+  //     const sel = users.filter((user) => {
+  //       console.log(user.email);
+  //       return user.email === auth.currentUser.email;
+  //     });
+  //     return sel[0];
+  //   }
+  // }
 
-
-  onAuthStateChanged(auth, (currentUser) => {
-    setUser(currentUser);
-  });
-
+  // onAuthStateChanged(auth, (currentUser) => {
+  //   setUser(currentUser);
+  // });
 
   return (
     <>
       <AuthContextProvider>
-        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        <Routes>
-          {/* <UnauthenticatedRoute exact path="/signup" component={} /> */}
-          <UnauthenticatedRoute exact path="/login" component={<LoginContainer />} />
-          <AuthenticatedRoute exact path="/home" component={<HomeContainer />} />
-          <AuthenticatedRoute exact path="/feed" component={<FeedContainer />} />
-          <AuthenticatedRoute exact path="/profile" component={<ProfileContainer />} />
+        <Router>
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <Routes>
+            <UnauthenticatedRoute path="/login" element={<LoginContainer />} />
+            {/* <UnauthenticatedRoute path="/signup" component={<Signup />} />
+            <AuthenticatedRoute path="/" component={<HomeContainer />} /> */}
+            {/* <AuthenticatedRoute exact path="/feed" component={<FeedContainer />} />
+            <AuthenticatedRoute exact path="/profile" component={<ProfileContainer />} /> */}
 
-
-
-
-          {/* <Route path="/" element={<Navigate to="/login" />} /> */}
-          {/* <Route path="/login" element={<LoginContainer />} /> */}
-          {/* <Route
-            path="/home"
-            element={user ? <HomeContainer /> : <Navigate to="/login" />}
-          /> */}
-          {/* <Route
-            path="/feed"
-            element={
-              user ? <FeedContainer auth={auth} /> : <Navigate to="/login" />
-            }
-          />
-          <Route
-            path="/profile"
-            element={<ProfileContainer user={auth.currentUser} />}
-          /> */}
-        </Routes>
+            {/* <Route path="/" element={<Navigate to="/login" />} /> */}
+            {/* <Route path="/login" element={<LoginContainer />} /> */}
+            {/* <Route
+              path="/home"
+              element={user ? <HomeContainer /> : <Navigate to="/login" />}
+            /> */}
+            {/* <Route
+              path="/feed"
+              element={
+                user ? <FeedContainer auth={auth} /> : <Navigate to="/login" />
+              }
+            />
+            <Route
+              path="/profile"
+              element={<ProfileContainer user={auth.currentUser} />}
+            /> */}
+          </Routes>
+        </Router>
       </AuthContextProvider>
-      <button onClick={getLoggedIn}>click me</button>
+      {/* <button onClick={getLoggedIn}>click me</button> */}
     </>
   );
 }
--- client/src/auth/AuthContext.js
+++ client/src/auth/AuthContext.js
@@ -1,20 +1,27 @@
 import { getAuth, onAuthStateChanged } from '@firebase/auth';
 import { useState, useEffect, useContext, createContext } from 'react';
 
-export const AuthContext = createContext()
+// Holding the state of the user that is currently signed in
+export const AuthContext = createContext();
 
+// Pass in the state
 export const AuthContextProvider = props => {
-  const [user, setUser] = useState()
-  const [error, setError] = useState()
+  // Passin g
+  const [user, setUser] = useState();
+  const [error, setError] = useState();
 
+  // When a component is unmounted, we will want to make sure that we unsubscribe
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(getAuth(), setUser, setError)
-    return () => unsubscribe()
+    const unsubscribe = onAuthStateChanged(getAuth(), setUser, setError);
+    return () => unsubscribe();
   }, [])
-  return <AuthContext.Provider value={{ user, error }} {...props} />
+
+  // Passing in the state as a value
+  return <AuthContext.Provider value={{ user, error }} {...props} />;
 }
 
+// This function returns the context, default sent to
 export const useAuthState = () => {
-  const auth = useContext(AuthContext)
-  return { ...auth, isAuthenticated: auth.user != null }
+  const auth = useContext(AuthContext);
+  return { ...auth, isAuthenticated: auth.user != null };
 }
