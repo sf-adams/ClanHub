4387a61131f89b088eb74ca3cabfb7806d83b8c8
Sam <samuel.fitz@btinternet.com>
2022-01-27 17:09:25 +0000

    Returning App.js Items

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,38 @@
 import "./styles/css/style.css";
-// import { useState, useEffect } from "react";
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Routes,
-//   Link
-// } from 'react-router-dom';
-// import { Navigate } from "react-router-dom";
-// import UserService from './services/UserService';
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from 'react-router-dom';
+import { Navigate } from "react-router-dom";
+import UserService from './services/UserService';
 
-// // Container & Component Imports
-// import Navbar from "./components/navbar/Navbar";
-// import Menu from "./components/navbar/Menu";
-// import FeedContainer from "./containers/FeedContainer";
-// import ProfileContainer from "./containers/ProfileContainer";
-// import LoginContainer from "./containers/LoginContainer";
-// import HomeContainer from "./containers/HomeContainer";
+// Container & Component Imports
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/navbar/Menu";
+import FeedContainer from "./containers/FeedContainer";
+import ProfileContainer from "./containers/ProfileContainer";
+import LoginContainer from "./containers/LoginContainer";
+import Signup from "./components/login/Signup";
+import HomeContainer from "./containers/HomeContainer";
 
-// // Authentication Imports
-// import { auth } from "./auth/firebase-config";
-// import { AuthContextProvider } from "./auth/AuthContext";
-// // import { AuthenticatedRoute } from "./auth/AuthenticatedRoute";
-// // import { UnauthenticatedRoute } from "./auth/UnauthenticatedRoute";
-// import { onAuthStateChanged } from "firebase/auth";
-// import Signup from "./components/login/Signup";
+// Authentication Imports
+import { auth } from "./auth/firebase-config";
+import { AuthContextProvider } from "./auth/AuthContext";
+import { AuthenticatedRoute } from "./auth/AuthenticatedRoute";
+import { UnauthenticatedRoute } from "./auth/UnauthenticatedRoute";
+import { onAuthStateChanged } from "firebase/auth";
+
 
 
 function App() {
 
-//   const [user, setUser] = useState({});
-//   const [loggedIn, setLoggedIn] = useState({})
-//   const [users, setUsers] = useState([]);
-//   const [menuOpen, setMenuOpen] = useState(false);
+  const [user, setUser] = useState({});
+  const [loggedIn, setLoggedIn] = useState({})
+  const [users, setUsers] = useState([]);
+  const [menuOpen, setMenuOpen] = useState(false);
 
 //   const AuthenticatedRoute = ({ component: C, ...props }) => {
 //     const { isAuthenticated } = useAuthState()
@@ -77,46 +78,55 @@ function App() {
 //   //   }
 //   // }
 
-//   // onAuthStateChanged(auth, (currentUser) => {
-//   //   setUser(currentUser);
-//   // });
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
 
   return (
     <>
       Hello world
     </>
-//     //   <AuthContextProvider>
-//     //     <Router>
-//     //       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-//     //       <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-//     //       <Routes>
-//     //         <UnauthenticatedRoute path="/login" element={<LoginContainer />} />
-//     //         {/* <UnauthenticatedRoute path="/signup" component={<Signup />} />
-//     //         <AuthenticatedRoute path="/" component={<HomeContainer />} /> */}
-//     //         {/* <AuthenticatedRoute exact path="/feed" component={<FeedContainer />} />
-//     //         <AuthenticatedRoute exact path="/profile" component={<ProfileContainer />} /> */}
 
-//     //         {/* <Route path="/" element={<Navigate to="/login" />} /> */}
-//     //         {/* <Route path="/login" element={<LoginContainer />} /> */}
-//     //         {/* <Route
-//     //           path="/home"
-//     //           element={user ? <HomeContainer /> : <Navigate to="/login" />}
-//     //         /> */}
-//     //         {/* <Route
-//     //           path="/feed"
-//     //           element={
-//     //             user ? <FeedContainer auth={auth} /> : <Navigate to="/login" />
-//     //           }
-//     //         />
-//     //         <Route
-//     //           path="/profile"
-//     //           element={<ProfileContainer user={auth.currentUser} />}
-//     //         /> */}
-//     //       </Routes>
-//     //     </Router>
-//     //   </AuthContextProvider>
-//     //   {/* <button onClick={getLoggedIn}>click me</button> */}
-//     // </>
+
+
+
+
+
+
+
+
+    //   <AuthContextProvider>
+    //     <Router>
+    //       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+    //       <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+    //       <Routes>
+    //         <UnauthenticatedRoute path="/login" element={<LoginContainer />} />
+    //         {/* <UnauthenticatedRoute path="/signup" component={<Signup />} />
+    //         <AuthenticatedRoute path="/" component={<HomeContainer />} /> */}
+    //         {/* <AuthenticatedRoute exact path="/feed" component={<FeedContainer />} />
+    //         <AuthenticatedRoute exact path="/profile" component={<ProfileContainer />} /> */}
+
+    //         {/* <Route path="/" element={<Navigate to="/login" />} /> */}
+    //         {/* <Route path="/login" element={<LoginContainer />} /> */}
+    //         {/* <Route
+    //           path="/home"
+    //           element={user ? <HomeContainer /> : <Navigate to="/login" />}
+    //         /> */}
+    //         {/* <Route
+    //           path="/feed"
+    //           element={
+    //             user ? <FeedContainer auth={auth} /> : <Navigate to="/login" />
+    //           }
+    //         />
+    //         <Route
+    //           path="/profile"
+    //           element={<ProfileContainer user={auth.currentUser} />}
+    //         /> */}
+    //       </Routes>
+    //     </Router>
+    //   </AuthContextProvider>
+    //   {/* <button onClick={getLoggedIn}>click me</button> */}
+    // </>
 
 //     <AuthContextProvider>
 //     <Router>
