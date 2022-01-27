0063a1dc2deeb1af1b372e14a3036f0089ec7deb
Sam <samuel.fitz@btinternet.com>
2022-01-27 16:42:40 +0000

    Amended Mistake in firebase config file

--- client/src/App.js
+++ client/src/App.js
@@ -1,95 +1,134 @@
 import "./styles/css/style.css";
-import { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link,
-  Redirect
-} from 'react-router-dom';
-import { Navigate } from "react-router-dom";
-import UserService from './services/UserService';
+// import { useState, useEffect } from "react";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+//   Link
+// } from 'react-router-dom';
+// import { Navigate } from "react-router-dom";
+// import UserService from './services/UserService';
 
-// Container & Component Imports
-import Navbar from "./components/navbar/Navbar";
-import Menu from "./components/navbar/Menu";
-import FeedContainer from "./containers/FeedContainer";
-import ProfileContainer from "./containers/ProfileContainer";
-import LoginContainer from "./containers/LoginContainer";
-import HomeContainer from "./containers/HomeContainer";
+// // Container & Component Imports
+// import Navbar from "./components/navbar/Navbar";
+// import Menu from "./components/navbar/Menu";
+// import FeedContainer from "./containers/FeedContainer";
+// import ProfileContainer from "./containers/ProfileContainer";
+// import LoginContainer from "./containers/LoginContainer";
+// import HomeContainer from "./containers/HomeContainer";
 
-// Authentication Imports
-import { auth } from "./auth/firebase-config";
-import { AuthContextProvider } from "./auth/AuthContext";
-import { AuthenticatedRoute } from "./auth/AuthenticatedRoute";
-import { UnauthenticatedRoute } from "./auth/UnauthenticatedRoute";
-import { onAuthStateChanged } from "firebase/auth";
-import Signup from "./components/login/Signup";
+// // Authentication Imports
+// import { auth } from "./auth/firebase-config";
+// import { AuthContextProvider } from "./auth/AuthContext";
+// // import { AuthenticatedRoute } from "./auth/AuthenticatedRoute";
+// // import { UnauthenticatedRoute } from "./auth/UnauthenticatedRoute";
+// import { onAuthStateChanged } from "firebase/auth";
+// import Signup from "./components/login/Signup";
 
 
 function App() {
 
-  const [user, setUser] = useState({});
-  const [loggedIn, setLoggedIn] = useState({})
-  const [users, setUsers] = useState([]);
-  const [menuOpen, setMenuOpen] = useState(false);
+//   const [user, setUser] = useState({});
+//   const [loggedIn, setLoggedIn] = useState({})
+//   const [users, setUsers] = useState([]);
+//   const [menuOpen, setMenuOpen] = useState(false);
 
-  // useEffect(()=> {
-  //   UserService.getUsers().then((users)=> setUsers(users.data))
-  //   }, [])
+//   const AuthenticatedRoute = ({ component: C, ...props }) => {
+//     const { isAuthenticated } = useAuthState()
+//     console.log(`AuthenticatedRoute: ${isAuthenticated}`)
+//     return (
+//       <Route
+//         {...props}
+//         render={routeProps =>
+//           isAuthenticated ? <C {...routeProps} /> : <Navigate to="/login" />
+//         }
+//       />
+//     )
+//   }
 
-  // useEffect(()=> {
-  //   setLoggedIn(getLoggedIn)
-  // }, [user])
+//   const UnauthenticatedRoute = ({ component: C, ...props }) => {
+//     const { isAuthenticated } = useAuthState()
+//     console.log(`UnauthenticatedRoute: ${isAuthenticated}`)
+//     return (
+//       <Route
+//         {...props}
+//         render={routeProps =>
+//           !isAuthenticated ? <C {...routeProps} /> : <Navigate to="/" />
+//         }
+//       />
+//     )
+//   }
 
-  // const getLoggedIn = ()=> {
-  //   if (users && user){
-  //     const sel = users.filter((user) => {
-  //       console.log(user.email);
-  //       return user.email === auth.currentUser.email;
-  //     });
-  //     return sel[0];
-  //   }
-  // }
+//   // useEffect(()=> {
+//   //   UserService.getUsers().then((users)=> setUsers(users.data))
+//   //   }, [])
 
-  // onAuthStateChanged(auth, (currentUser) => {
-  //   setUser(currentUser);
-  // });
+//   // useEffect(()=> {
+//   //   setLoggedIn(getLoggedIn)
+//   // }, [user])
+
+//   // const getLoggedIn = ()=> {
+//   //   if (users && user){
+//   //     const sel = users.filter((user) => {
+//   //       console.log(user.email);
+//   //       return user.email === auth.currentUser.email;
+//   //     });
+//   //     return sel[0];
+//   //   }
+//   // }
+
+//   // onAuthStateChanged(auth, (currentUser) => {
+//   //   setUser(currentUser);
+//   // });
 
   return (
     <>
-      <AuthContextProvider>
-        <Router>
-          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-          <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-          <Routes>
-            <UnauthenticatedRoute path="/login" element={<LoginContainer />} />
-            {/* <UnauthenticatedRoute path="/signup" component={<Signup />} />
-            <AuthenticatedRoute path="/" component={<HomeContainer />} /> */}
-            {/* <AuthenticatedRoute exact path="/feed" component={<FeedContainer />} />
-            <AuthenticatedRoute exact path="/profile" component={<ProfileContainer />} /> */}
-
-            {/* <Route path="/" element={<Navigate to="/login" />} /> */}
-            {/* <Route path="/login" element={<LoginContainer />} /> */}
-            {/* <Route
-              path="/home"
-              element={user ? <HomeContainer /> : <Navigate to="/login" />}
-            /> */}
-            {/* <Route
-              path="/feed"
-              element={
-                user ? <FeedContainer auth={auth} /> : <Navigate to="/login" />
-              }
-            />
-            <Route
-              path="/profile"
-              element={<ProfileContainer user={auth.currentUser} />}
-            /> */}
-          </Routes>
-        </Router>
-      </AuthContextProvider>
-      {/* <button onClick={getLoggedIn}>click me</button> */}
+      Hello world
     </>
+//     //   <AuthContextProvider>
+//     //     <Router>
+//     //       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+//     //       <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+//     //       <Routes>
+//     //         <UnauthenticatedRoute path="/login" element={<LoginContainer />} />
+//     //         {/* <UnauthenticatedRoute path="/signup" component={<Signup />} />
+//     //         <AuthenticatedRoute path="/" component={<HomeContainer />} /> */}
+//     //         {/* <AuthenticatedRoute exact path="/feed" component={<FeedContainer />} />
+//     //         <AuthenticatedRoute exact path="/profile" component={<ProfileContainer />} /> */}
+
+//     //         {/* <Route path="/" element={<Navigate to="/login" />} /> */}
+//     //         {/* <Route path="/login" element={<LoginContainer />} /> */}
+//     //         {/* <Route
+//     //           path="/home"
+//     //           element={user ? <HomeContainer /> : <Navigate to="/login" />}
+//     //         /> */}
+//     //         {/* <Route
+//     //           path="/feed"
+//     //           element={
+//     //             user ? <FeedContainer auth={auth} /> : <Navigate to="/login" />
+//     //           }
+//     //         />
+//     //         <Route
+//     //           path="/profile"
+//     //           element={<ProfileContainer user={auth.currentUser} />}
+//     //         /> */}
+//     //       </Routes>
+//     //     </Router>
+//     //   </AuthContextProvider>
+//     //   {/* <button onClick={getLoggedIn}>click me</button> */}
+//     // </>
+
+//     <AuthContextProvider>
+//     <Router>
+//       <div>
+//         <Link to="/">Home</Link> | <Link to="/login">Login</Link> |{' '}
+//         <Link to="/signup">SignUp</Link>
+//       </div>
+//       <AuthenticatedRoute exact path="/" component={HomeContainer} />
+//       {/* <UnauthenticatedRoute exact path="/signup" component={S} /> */}
+//       <UnauthenticatedRoute exact path="/login" component={LoginContainer} />
+//     </Router>
+//   </AuthContextProvider>
   );
 }
 
--- client/src/auth/firebase-config.js
+++ client/src/auth/firebase-config.js
@@ -1,8 +1,8 @@
-import { initializeApp } from "firebase/app";
+import firebase from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 // Firebase details being read from .env file
-const firebaseConfig = {
+const app = firebase.initializeApp ({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
@@ -10,10 +10,11 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
-};
+});
 
-// Exporting the app so it can be used elsewhere
-export const app = initializeApp(firebaseConfig);
+
 
 // The auth variable creates an authentication instance of app
 export const auth = getAuth(app);
+
+export default app;
