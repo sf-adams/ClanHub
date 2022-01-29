b42439ff3c51d6504ac0dbabc402dca364dfc4b2
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-01-29 11:09:06 +0000

    refacored profile contaianer

--- client/src/containers/ProfileContainer.js
+++ client/src/containers/ProfileContainer.js
@@ -5,50 +5,12 @@ import ProfileHeader from "../components/profile/ProfileHeader";
 import ProfileDetails from "../components/profile/ProfileDetails";
 import ProfileHistoryList from "../components/profile/ProfileHistoryList";
 
-function ProfileContainer({ user, loggedIn, posts }) {
+function ProfileContainer({ user, posts }) {
   return (
     <div className="profile-container">
       <ProfileHeader user={user} />
-      <ProfileDetails user={user} loggedIn={loggedIn} />
+      <ProfileDetails user={user} />
       <ProfileHistoryList user={user} posts={posts} />
-
-        
-function ProfileContainer({user, loggedIn,}) {
-  // const [loginRef, setLoginRef] = useRef(loggedIn);
-
-  function namedPhoto(){
-    if(loggedIn) {
-    return `${loggedIn?.firstName} ${loggedIn?.lastName}`
-    }
-  }
-  
-  return (
-    <div className="profile-container">
-      <h1>This is the profile page.</h1>
-
-      <p>{user?.email}</p>
-
-      <div className="profileRight">
-        <div className="profileRightTop">
-          <div className="profileCover">
-            <img className="profileCoverImg" src="assets/post/3.jpeg" alt="" />
-            <img className="profileUserImg" src={blankProfile} alt="" />
-          </div>
-          <div className="profileInfo">
-            <h4 className="profileInfoName">{namedPhoto()}</h4>
-            <span className="profileInfoDesc">I'm the first user!</span>
-          </div>
-        </div>
-      </div>
-      <ul>
-        <li>{loggedIn?.firstName}</li>
-        <li>{loggedIn?.lastName}</li>
-        <li>{loggedIn?.email}</li>
-        <li>{loggedIn?.bio}</li>
-        <li>{loggedIn?.linkedin}</li>
-        <li>{loggedIn?.github}</li>
-      </ul>
-
       <Link to="/feed">Click to view your feed.</Link>
     </div>
   );
