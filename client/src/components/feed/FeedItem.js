e311d904806614ed2c29a68b98856a74f47e9aa9
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-01-29 10:45:46 +0000

    added new post to feed

--- client/src/components/feed/FeedItem.js
+++ client/src/components/feed/FeedItem.js
@@ -1,15 +1,14 @@
 import React from "react";
 
 const FeedItem = ({ post }) => {
-
-
   return (
     <>
       <div className="feed-item-object">
         <h4 className="feed-item-object-title">{post.title}</h4>
         <p className="feed-item-object-description">{post.description}</p>
-        {/* <p className="feed-item-object-author">{post.user.firstName} {post.user.lastName}
-  </p> */}
+        <p className="feed-item-object-author">
+          {post.user?.firstName} {post.user?.lastName}
+        </p>
       </div>
     </>
   );
