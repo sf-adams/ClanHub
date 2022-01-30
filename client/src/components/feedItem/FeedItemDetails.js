8c5eda3142275c90651f78f47c5a2190522cbed8
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-01-30 19:41:44 +0000

    added information for feed details

--- client/src/components/feedItem/FeedItemDetails.js
+++ client/src/components/feedItem/FeedItemDetails.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const FeedItemDetails= ({post})=> {
   return (
-      <p>{post.id}</p>
+      <div className="post-details-container">
+          <h3 className="post-details-header">{post.title}</h3>
+          <p>{post.user.firstName} {post.user.lastName}</p>
+          <p>{post.description}</p>
+      </div>
   );
 }
 
