300d7f2002afb3d893aa1688b9b7a22abeb579b7
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-01-31 12:17:49 +0000

    comments now display

--- client/src/components/feedItem/comments/Comment.js
+++ client/src/components/feedItem/comments/Comment.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Comment = () => {
-  return (<p>Hello world</p>);
+const Comment = (comment) => {
+  
+  const handleClick = () => {
+      console.log(comment.comment)
+  };
+  
+    return (
+      <div className="comment-wrapper">
+          <h3 className="comment-title">{comment.comment.body}</h3>
+          <button onClick = {handleClick}></button>
+      </div>
+  );
 };
 
 export default Comment;
