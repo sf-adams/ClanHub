4bd99d34eef6a98b8ac0be959ff280f2f05dca31
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-02-01 20:48:47 +0000

    basic styling on feed page

--- client/src/components/feed/FeedItem.js
+++ client/src/components/feed/FeedItem.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {useNavigate} from 'react-router-dom'
 import { Link } from "react-router-dom";
 import FeedEditPostPopUp from "./FeedEditPostPopUp";
 import FeedItemDeletePopUp from "./FeedItemDeletePopUp";
@@ -7,11 +8,14 @@ import PostService from "../../services/PostService";
 
 import { FiEdit3 } from "react-icons/fi";
 import { MdDeleteOutline } from "react-icons/md";
+import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai";
 
 const FeedItem = ({ post, posts, user, deletePost, updatePost }) => {
   const [toggleEdit, setToggleEdit] = useState(false);
   const [toggleDeletionAlert, setToggleDeletionAlert] = useState(false);
 
+
+  const navigate = useNavigate();
   // () => {
   //               deletePost(post?.id)
   const handleToggleEdit = () => {
@@ -41,25 +45,36 @@ const FeedItem = ({ post, posts, user, deletePost, updatePost }) => {
 
   return (
     <>
-      <div className="feed-item-object">
-        <h4 className="feed-item-object-title">{post.title}</h4>
-        <p className="feed-item-object-author">
-          {post.user?.firstName} {post.user?.lastName}
-        </p>
-        <p>{post.time}</p>
-        <p className="feed-item-object-description">{post.description}</p>
-        {toggleEdit ? (
-          <FeedEditPostPopUp
-            post={post}
-            posts={posts}
-            handleToggleEdit={handleToggleEdit}
-            setData={setData}
-            updatePost={updatePost}
-          />
-        ) : null}
-        <Link className="feed-item-link" to={`/feed/${post?.id}`} post={post}>
-          See post
-        </Link>
+      <section onClick = {()=> {navigate(`/feed/${post?.id}`)}} className="feed-item-object">
+        <div className="feed-item-upvotes-wrapper">
+          <AiOutlineArrowUp className="feed-item-upvotes" />
+          <span className="feed-item-upvotes ">{post.upvotes}</span>
+          <AiOutlineArrowDown className="feed-item-upvotes" />
+        </div>
+        <div className="feed-item-object-text-wrapper">
+          <h4 className="feed-item-object-title">{post.title}</h4>
+          <p className="feed-item-object-description">{post.description}</p>
+          <hr className="line-break"/>
+          <div className="feed-item-object-metadata">
+            <p className="feed-item-object-author">
+              {post.user?.firstName} {post.user?.lastName}
+            </p>
+            <p>{post.time}</p>
+          </div>
+
+          {toggleEdit ? (
+            <FeedEditPostPopUp
+              post={post}
+              posts={posts}
+              handleToggleEdit={handleToggleEdit}
+              setData={setData}
+              updatePost={updatePost}
+            />
+          ) : null}
+          <Link className="feed-item-link" to={`/feed/${post?.id}`} post={post}>
+            See post
+          </Link>
+        </div>
         {user?.email == post.user?.email ? (
           <div className="feed-item-buttons">
             <FiEdit3
@@ -79,7 +94,7 @@ const FeedItem = ({ post, posts, user, deletePost, updatePost }) => {
         {toggleDeletionAlert ? (
           <FeedItemDeletePopUp confirmDeletion={confirmDeletion} />
         ) : null}
-      </div>
+      </section>
     </>
   );
 };
